837bbee4a8647ae5627e29406fbeee14996da491
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2024-02-22 23:54:01 +0530

    add heroSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main>
+    <main className=" min-h-screen bg-black/[0.96] antialiased">
       <h1>Hello</h1>
       <HeroSection/>
     </main>
