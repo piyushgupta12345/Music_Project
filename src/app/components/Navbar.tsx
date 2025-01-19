6bb766f6382ed738b3131f83e85cb2bd4509680d
Piyush Gupta <pg261610@gmail.com>
2025-01-19 16:39:23 +0530

    code updated

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem} from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "../utils/cn";
 import Link from "next/link";
 
@@ -11,25 +11,30 @@ function Navbar({ className }: { className?: string })
         <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
             <Menu setActive={setActive}>
                 <Link href={'/'}>
-                <MenuItem setActive={setActive} active={active} item="Home">
-                    <div className="flex flex-col space-y-4 text-sm">
-                    </div>
-                </MenuItem>
+                    <MenuItem setActive={setActive} active={active} item="Home">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/about">About</HoveredLink>
+                        </div>
+                    </MenuItem>
                 </Link>
                 <Link href={'/'}>
-                <MenuItem setActive={setActive} active={active} item="Our Courses">
-                <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/courses">All Courses</HoveredLink>
-                        <HoveredLink href="/individual">Basic Music Theory</HoveredLink>
-                        <HoveredLink href="/team">Advanced Composition</HoveredLink>
-                        <HoveredLink href="/enterprise">Songwriting</HoveredLink>
-                        <HoveredLink href="/enterprise">Music Production</HoveredLink>
-                    </div>
-                </MenuItem>
+                    <MenuItem setActive={setActive} active={active} item="Our Courses">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/courses">All Courses</HoveredLink>
+                            <HoveredLink href="/individual">Basic Music Theory</HoveredLink>
+                            <HoveredLink href="/team">Advanced Composition</HoveredLink>
+                            <HoveredLink href="/enterprise">Songwriting</HoveredLink>
+                            <HoveredLink href="/enterprise">Music Production</HoveredLink>
+                        </div>
+                    </MenuItem>
                 </Link>
                 <Link href={'/contact'}>
-                <MenuItem setActive={setActive} active={active} item="Contact Us">
-                </MenuItem>
+                    <MenuItem setActive={setActive} active={active} item="Contact Us">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/number">By Number</HoveredLink>
+                            <HoveredLink href="/mail">By Mail</HoveredLink>
+                        </div>
+                    </MenuItem>
                 </Link>
             </Menu>
         </div>
